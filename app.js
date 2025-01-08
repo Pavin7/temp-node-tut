37b2a3327ebbf2e0a80b561fa3210f050961e535
Pavin7 <pavinwere10@gmail.com>
2025-01-08 22:33:27 +0300

    Initial commit

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
+const __ = require('lodash')
 
-const http = require('http')
-
-const server = http.createServer((req,res)=>{
-    
-
-    if(req.url==='/'){
-        res.end('welcome to our homepage')
-    }
-
-    if(req.ur1==='/about'){
-        res.end('Here is our short story ')
-    }
-
-    res.end(
-        `<h1>OOPS</h1>
-        <p>wecant find what you want j</p>`
+const items = []
 
 
-    )
-
-    
-
-}
-
-)
-
-server.listen(5000)
